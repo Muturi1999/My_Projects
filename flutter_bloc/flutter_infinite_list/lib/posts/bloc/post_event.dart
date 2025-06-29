ed233d91ff2705e767b63f4952ad42c7c312a3b3
mikemuturi <mikeirungumuturi@gmail.com>
2025-06-29 19:49:29 +0300

    infinite list

---
+++ flutter_bloc/flutter_infinite_list/lib/posts/bloc/post_event.dart
@@ -0,0 +1,9 @@
+part of 'post_bloc.dart';
+// import 'package:equatable/equatable.dart';
+
+sealed class PostEvent extends Equatable {
+  @override
+  List<Object> get props => [];
+}
+
+final class PostFetched extends PostEvent {}
