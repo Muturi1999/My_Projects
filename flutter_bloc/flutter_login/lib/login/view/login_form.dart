e0000c727b53c1c7b1e45a8da5d2eb30814c7751
mikemuturi <mikeirungumuturi@gmail.com>
2025-07-01 11:42:56 +0300

    Completed login app for flutter

--- flutter_bloc/flutter_login/lib/login/view/login_form.dart
+++ flutter_bloc/flutter_login/lib/login/view/login_form.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_login/login/bloc/login_bloc.dart';
 import 'package:flutter_login/login/login.dart';
 import 'package:formz/formz.dart';
 
