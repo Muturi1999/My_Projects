9e02d735c33d10eb10c430632f68a706c0784b7c
mikemuturi <mikeirungumuturi@gmail.com>
2025-09-11 14:53:04 +0300

    travel-list react app

---
+++ react/travel-list/src/Stats.jsx
@@ -0,0 +1,25 @@
+ export default function Stats({items}) {
+
+  if (!items.length) return (
+    <p className="stats">
+      <em>Start adding items to your packing list.</em>
+    </p>
+  )
+  const numItems = items.length;
+  // const numItems = items.length;
+  const packedItems = items.filter((item) => item.packed);
+  const numPacked = packedItems.length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
+  return (
+    <footer className="stats">
+      <em>
+  {percentage === 100
+    ? "You got everything! Ready to go! 🎉"
+    : `You have ${numItems} items on your list, and you already packed ${numPacked} (${percentage}%)`}
+</em>
+
+
+    </footer>
+  );
+}
